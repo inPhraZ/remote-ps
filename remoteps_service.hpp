3719bdd1d9a87c6b093f9c60946f84febd89639d
Farzin <monseffarzin@gmail.com>
2022-07-20 13:07:01 +0430

    service class

---
+++ remoteps_service.hpp
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  remoteps_service.hpp
+ *
+ *    Description:  Definiton of RemotePsService class.
+ *    				Logic and data behind the server's behavior.
+ *
+ *         Author:  Farzin Monsef 
+ *
+ * =====================================================================================
+ */
+
+#ifndef 		__REMOTEPS_SERVICE_HPP_
+#define 		__REMOTEPS_SERVICE_HPP_		1
+
+class RemotePsService
+{
+}
+
+
+#endif 		/*  __REMOTEPS_SERVICE_HPP_		*/
