ca7a3af684431507bd45c19f899148a3810f0817
Farzin <monseffarzin@gmail.com>
2022-07-20 19:36:10 +0430

    version

---
+++ version.hpp
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  version.hpp
+ *
+ *    Description:  remoteps version.hpp configuration header file
+ *
+ *         Author:  Farzin Monsef 
+ *
+ * =====================================================================================
+ */
+
+#ifndef 	__REMOTEPS_VERSION_HPP_
+#define 	__REMOTEPS_VERSION_HPP_		1
+
+//	remoteps version number
+//	x.y		MAJOR.MINOR
+#define 	REMOTEPS_VERSION	"1.0"
+
+#endif	/*  __REMOTEPS_VERSION_HPP_ */
