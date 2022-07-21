539bd89f2117267295886b0050b2fb1d21646f9e
Farzin <monseffarzin@gmail.com>
2022-07-21 18:33:30 +0430

    RemotePs cunstructor

--- remoteps_client.cpp
+++ remoteps_client.cpp
@@ -10,4 +10,16 @@
  * =====================================================================================
  */
 
+#include <string>
+#include <memory>
+
 #include "remoteps_client.hpp"
+
+remoteps::RemotePsClient::RemotePsClient(const std::string& ip, const uint16_t port)
+{
+	peer.setIP(ip);
+	peer.setPort(port);
+	auto channel_creds = grpc::SslCredentials(grpc::SslCredentialsOptions());
+	channel = grpc::CreateChannel(peer.IpPort(), channel_creds);
+	stub_ = RemotePs::NewStub(channel);
+}
