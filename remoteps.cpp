86e9a3cdfdd3e7b7980efad1422e63f6771175e8
Farzin <monseffarzin@gmail.com>
2022-07-21 19:23:20 +0430

    parse_command_line

--- remoteps.cpp
+++ remoteps.cpp
@@ -12,15 +12,75 @@
 
 #include <iostream>
 #include <string>
+#include <exception>
+#include <cstdint>
+#include <cerrno>
+#include <boost/program_options.hpp>
 
 #include "remoteps_client.hpp"
+#include "version.hpp"
 
+namespace po = boost::program_options;
 
-int main()
+void parse_command_line(int argc, char *argv[], std::string& ip, uint16_t& port);
+
+int main(int argc, char *argv[])
 {
-	std::string target_str("0.0.0.0:5000");
-	remoteps::RemotePsClient client(
-			grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
-	std::cout << "Hello from client\n";
+	std::string ip;
+	uint16_t    port;
+
+	remoteps::RemotePsClient client(ip, port);
+
 	return 0;
+}
+
+void parse_command_line(int argc, char *argv[], std::string& ip, uint16_t& port)
+{
+	try {
+		po::options_description description("[OPTIONS]");
+		description.add_options()
+			("ip,i",
+			 po::value<std::string>()->default_value("0.0.0.0"),
+			 "specify IP address of remote server")
+			("port,p",
+			 po::value<uint16_t>()->default_value(5000),
+			 "specify PORT number of remote server")
+			("help,h", "display this help and exit")
+			("version,v", "output version information and exit");
+	
+		po::variables_map vm;
+		po::store(po::parse_command_line(argc, argv, description), vm);
+		po::notify(vm);
+
+		if (vm.count("help")) {
+			std::cout << "Usage: " << program_invocation_name << " [OPTIONS]" << std::endl;
+			std::cout << "Report information of the current "
+				"processes on a remote linux server\n" << std::endl;
+			std::cout << description;
+			exit(EXIT_SUCCESS);
+		}
+
+		if (vm.count("version")) {
+			std::cout << REMOTEPS_NAME << " " << REMOTEPS_VERSION << std::endl;
+			std::cout << "Written by " << REMOTEPS_AUTHOR << std::endl;
+			exit(EXIT_SUCCESS);
+		}
+
+		if (vm.count("port")) {
+			if (vm["port"].as<uint16_t>() <= 0) {
+				std::cerr << "Invalid port number: " << vm["port"].as<uint16_t>() << std::endl;
+				exit(EXIT_FAILURE);
+			}
+		}
+		ip = vm["ip"].as<std::string>();
+		port = vm["port"].as<uint16_t>();
+	}
+	catch (std::exception& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	catch (...) {
+		std::cerr << "Unknown exception" << std::endl;
+		exit(EXIT_FAILURE);
+	}
 }
