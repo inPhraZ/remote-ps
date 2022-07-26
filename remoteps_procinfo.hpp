a5bdc06bbb39945a112f191cc8b238560af8dd50
Farzin <monseffarzin@gmail.com>
2022-07-26 19:16:54 +0430

    process information

---
+++ remoteps_procinfo.hpp
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  remoteps_procinfo.hpp
+ *
+ *    Description:  Definition of Process Information class.
+ *                  extract information of running procs on machine.
+ *
+ *         Author:  Farzin Monsef 
+ *
+ * =====================================================================================
+ */
+
+#ifndef 		__REMOTEPS_PROCINFO_HPP_
+#define 		__REMOTEPS_PROCINFO_HPP_	1
+
+#include <vector>
+
+#include "remoteps.pb.h"
+
+namespace remoteps {
+	class ProcInfo
+	{
+		private:
+			std::vector<Process> procs;
+	};
+}
+
+#endif 		/*  __REMOTEPS_PROCINFO_HPP_	*/
